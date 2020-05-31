e1bdc6cab2dc9732fdd989c4320cadde52c6abdc
Mark Qvist <mark@adepta.io>
2020-05-31 12:16:50 +0200

    Added macOS buildscript

---
+++ buildscript_macos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#rm -rf cool-retro-term.app
+
+# For macOS
+export CPPFLAGS="-I/usr/local/opt/qt5/include"
+export LDFLAGS="-L/usr/local/opt/qt5/lib"
+export PATH=/usr/local/opt/qt5/bin:$PATH
+
+#make clean
+qmake && make
+cp -r qmltermwidget/src/qmldir qmltermwidget/lib/kb-layouts qmltermwidget/lib/color-schemes qmltermwidget/src/QMLTermScrollbar.qml qmltermwidget/QMLTermWidget
+
+# For macOS
+mkdir cool-retro-term.app/Contents/PlugIns
+cp -r qmltermwidget/QMLTermWidget cool-retro-term.app/Contents/PlugIns
